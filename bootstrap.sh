df23f96f8684f8b75855216cf973960f792530f5
Jason Rankin <rankin.jason@gmail.com>
2015-06-12 17:02:02 -0400

    bootstrap script to backup old dotfiles and create symlinks to the ones in this repo

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+###########################################################################################################
+# .make.sh
+# This script creates symlinks from the home directory to speficied files in the dotfiles directory
+###########################################################################################################
+
+dir=~/dotfiles                      # dotfiles directory
+olddir=~/dotfiles_old               # backup directory for old dotfiles
+files="vim zshrc tmux.conf"         # List of files/directories to link
+
+echo "Creating backup directory $olddir"
+mkdir -p $olddir
+echo "...done"
+
+echo "chdir to $dir"
+cd $dir
+echo "...done"
+
+for file in $files; do
+  echo "Moving existing file, $file, to $olddir"
+  mv ~/.$file $olddir
+  echo "Creating symlink to $file in $dir"
+  ln -s $dir/.$file ~/.$file
+done
+
